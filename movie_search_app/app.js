4a9737ca379f1412d1d0c0c38699a801f36d58bd
Hajredin Abidu <diniabdiu@gmail.com>
2019-04-16 16:23:02 +0200

    Displaying Data

--- movie_search_app/app.js
+++ movie_search_app/app.js
@@ -1,16 +1,46 @@
 var express = require('express');
 var app = express();
 var request = require('request');
+app.set('view engine', 'ejs');
 
 app.get('/results', function(req, res) {
-    request('http://www.omdbapi.com/?s=california&apikey=thewdb', function(error, response, body) {
+    request('http://www.omdbapi.com/?s=iowa&apikey=thewdb', function(error, response, body) {
         if(!error && response.statusCode == 200) {
-            var results = JSON.parse(body);
-
-            res.send(results['Search'][0]['Title']);
+            var data = JSON.parse(body);         
+            res.render('results', {data});
         }
+
+
     });
 });
+
 app.listen(3100, process.env.IP, function() {
     console.log('Movie app has started!');
 });
+
+
+// var express = require('express');
+// var app = express();
+// var request = require('request');
+// app.set('view engine', 'ejs');
+
+// app.get('/results', function(req, res) {
+//     res.render('results');
+// });
+
+// app.post('/results', function(req, res) {
+//     var newmoviee = req.params.newmovie;
+//     request(`http://www.omdbapi.com/?s=${newmoviee}&apikey=thewdb`, function(error, response, body) {
+//         if(!error && response.statusCode == 200) {
+//             var newmoviee = JSON.parse(body);         
+//         }
+//         newmoviee.push(newmovie);
+
+//     });
+//     res.render('/results', {newmoviee});
+// });
+
+// app.get()
+// app.listen(3100, process.env.IP, function() {
+//     console.log('Movie app has started!');
+// });
