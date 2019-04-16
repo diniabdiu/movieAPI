fd7db6252ad58013c18d6db30315f4cba45b6da6
Hajredin Abidu <diniabdiu@gmail.com>
2019-04-16 14:33:50 +0200

    .

--- movie_search_app/app.js
+++ movie_search_app/app.js
@@ -2,8 +2,15 @@ var express = require('express');
 var app = express();
 var request = require('request');
 
+app.get('/results', function(req, res) {
+    request('http://www.omdbapi.com/?s=california&apikey=thewdb', function(error, response, body) {
+        if(!error && response.statusCode == 200) {
+            var results = JSON.parse(body);
 
-
-app.listen(3000, process.env.IP, function() {
+            res.send(results['Search'][0]['Title']);
+        }
+    });
+});
+app.listen(3100, process.env.IP, function() {
     console.log('Movie app has started!');
 });
